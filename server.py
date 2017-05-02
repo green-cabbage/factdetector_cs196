cc618b0f6944460b24645afe3327e43f14d43eec
swissme8888 <hyeonseo777yun@gmail.com>
2017-05-03 00:09:54 +0200

    sdfds

--- server.py
+++ server.py
@@ -48,7 +48,7 @@ def upload_file():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             with open(os.path.join(UPLOAD_FOLDER, filename), 'r') as myfile:
-                data=unicode(myfile.read(), errors='ignore').replace("\n", "").replace("/", "").replace(",", "").replace("(", "").replace(")", "").replace("-", "").replace("\"", "").replace("'", "")
+                data=(myfile.read().encode(encoding='UTF-8',errors='strict').replace("\n", "").replace("/", "").replace(",", "").replace("(", "").replace(")", "").replace("-", "").replace("\"", "").replace("'", "")
             filedata1= data.split('.')
             #print(filedata1)
             #file 2
@@ -59,7 +59,7 @@ def upload_file():
             filename = secure_filename(twofile.filename)
             twofile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             with open(os.path.join(UPLOAD_FOLDER, filename), 'r') as myfile:
-                twodata=unicode(myfile.read(), errors='ignore').replace("\n", "").replace("/", "").replace(",", "").replace("(", "").replace(")", "").replace("-", "").replace("\"", "").replace("'", "")
+                twodata=myfile.read().encode(encoding='UTF-8',errors='strict').replace("\n", "").replace("/", "").replace(",", "").replace("(", "").replace(")", "").replace("-", "").replace("\"", "").replace("'", "")
             filedata2= twodata.split('.')
             #print(filedata2)
 
