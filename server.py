ca854b81c041b03e67ac33c557a116ef2d68a18f
swissme8888 <hyeonseo777yun@gmail.com>
2017-05-02 06:37:18 +0200

    integrated flipflop detector into server

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, request, redirect, url_for, send_from_directory,flash, render_template
 from werkzeug.utils import secure_filename
-#from flipper import *
+from flipflopDetector import *
 import json
 from watson_developer_cloud import AlchemyLanguageV1
 alchemy_language = AlchemyLanguageV1(api_key='cb738b85c0e2d0094894bcfe8c73d12d73543c35')
@@ -16,7 +16,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 ALLOWED_EXTENSIONS= set(['txt'])
 
 app.debug = True
-
+app.secret_key = 'some secret key'
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -37,14 +37,14 @@ def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
             flash('No file part')
-            return redirect(request.url)
+            #return redirect(request.url)
         file = request.files['file']
         twofile = request.files['twofile']
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
             flash('No selected file You idiot')
-            return redirect(request.url)
+            #return redirect(request.url)
         if file and allowed_file(file.filename):#triggered if the allowed file is uploaded
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
@@ -55,7 +55,7 @@ def upload_file():
             #file 2
         if twofile.filename == '':
             flash('No selected file You idiot')
-            return redirect(request.url)
+            #return redirect(request.url)
         if twofile and allowed_file(twofile.filename):#triggered if the allowed file is uploaded
             filename = secure_filename(twofile.filename)
             twofile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
@@ -70,10 +70,10 @@ def upload_file():
 
             bulkdata = filedata1 +filedata2
             print(bulkdata)
-            #retData=flipFlopped(bulkdata)
+            retData=flipFlopped(bulkdata)
+            print(retData)
             result =  {"# of fllipflops" : 1212, "# of consistencies":12321}
             return render_template("result.html",result = result)
-            #print(retData)
             # first cut file lenghts down  the concat them
 
 
